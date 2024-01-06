a2cb394c11a67b9cba7c27b515f325faf144ea57
Gabriel <Gabriel@192.168.0.105>
2024-01-06 19:08:54 -0200

    Testes de linha de codigo

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,5 +3,7 @@ package cursojava.executavel;
 public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Meu Primeiro Programa em java ");
+		System.out.println("Testando Projetos ja existente!");
+		
 	}
 }
