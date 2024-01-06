2a42e1d56154c2c0b089d84d9223a8d5bf364a84
Gabriel <Gabriel@192.168.0.105>
2024-01-06 19:33:45 -0200

    Revert "colocando "JOSE DE ARIMATEIRA"
    This reverts commit 807e7a0cba69528df77f57e81bf12af337cc8459.

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,7 +4,6 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Meu Primeiro Programa em java ");
 		System.out.println("Testando Projetos ja existente!");
-		System.out.println("JOSE DE ALMEIDA ARIMATAEIR ");
 		
 	}
 }
