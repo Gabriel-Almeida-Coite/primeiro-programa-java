ce4749ec71a26119e1cbbd9e1f8aef405fc0c02b
Gabriel <Gabriel@192.168.0.105>
2024-01-07 22:44:51 -0200

    TESTANTANDO GIT IGNORE

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,7 +4,5 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Meu Primeiro Programa em java ");
 		System.out.println("Testando Projetos ja existente!");
-		
-	
 	}
 }
